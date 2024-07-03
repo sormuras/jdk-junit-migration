23d885985a124cb1136fe369fee7fa9db36c8c4c
Christian Stein <sormuras@gmail.com>
2024-07-03 16:30:24 +0200

    Add verify program

---
+++ .bach/src/Verify.java
@@ -0,0 +1,28 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import run.bach.ToolCall;
+import run.tool.JTReg;
+
+public class Verify {
+  public static void main(String... args) throws Exception {
+    // Install JTReg tool
+    var jtreg = new JTReg().install();
+
+    // Always create a new target directory to ensure compilation
+    var timestamp =
+        OffsetDateTime.now(ZoneOffset.UTC)
+            .format(DateTimeFormatter.ofPattern("uuuuMMdd'T'HHmmssX"));
+    var temporary = Files.createDirectories(Path.of("tmp", timestamp));
+
+    // Run `jtreg`
+    ToolCall.of(jtreg)
+        .add("-agentvm")
+        .add("-r:" + temporary.resolve("JTReport"))
+        .add("-w:" + temporary.resolve("JTWork"))
+        .addAll(args)
+        .run();
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # jdk-junit-migration
+
+- Install JDK 22+ (better use the latest-and-greatest JDK EA Build from https://jdk.java.net)
+- Clone this repository with [Git Submodules](https://git-scm.com/book/en/v2/Git-Tools-Submodules)
+- Run `java @status` to show a summary of the current migration status
+- Run `java @verify Test.java` to execute a single test file
+- Run `java @migrate Test.java` to migrate a single test file in-place
+- Fix `Test.java` to remove wrong or add missing migration patterns and create [an issue](https://github.com/sormuras/jdk-junit-migration/issues/new/choose)
+- Run `java @check Test.java` to verify everything's still okay - if not, keep on fixing
+- Run `java @status` again to see migration progress
+
+Note that `Test.java` is usually prepended by a path to the test file, like `github/openjdk/jdk/test/jdk/jdk/nio/Basic.java` for `Basic.java`.
